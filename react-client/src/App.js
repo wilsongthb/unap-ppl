1a45742ca40bebf1ebaed076db860e39d6da954a
Wilson Pilco <wilsonaux1@gmail.com>
2022-12-13 22:36:15 -0500

    Correcciones menores

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+// import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Store from "./pages/Store.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="store" element={<Store />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar></Navbar>
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="store" element={<Store />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
