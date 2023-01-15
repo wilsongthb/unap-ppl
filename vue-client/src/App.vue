d3fe818b214d8e6f1ffa8dae5eac408a96cb264d
Wilson Pilco <wilsonaux1@gmail.com>
2023-01-15 16:43:28 -0500

    liberado app, creado public container

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <div class="container">
-    <!-- <nav class="mb-2"> -->
-    <!--   <router-link to="/">Home</router-link> | -->
-    <!--   <router-link to="/about">About</router-link> | -->
-    <!--   <router-link to="/store">Store</router-link> -->
-    <!-- </nav> -->
-    <div class="row">
-      <div class="col-12 col-sm-8">
-        <TopNavbar class="mb-2"></TopNavbar>
-        <router-view />
-      </div>
-    </div>
-  </div>
+  <router-view />
 </template>
 <script>
-import TopNavbar from "./components/TopNavbar.vue";
 export default {
   components: {
-    TopNavbar
+    //
   }
 };
 </script>
